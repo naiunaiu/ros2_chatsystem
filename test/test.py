2c9c4b06513567625b8645d3ab94b5531b618e15
naiu <imagenaru2@gmail.com>
2025-12-31 22:52:43 +0900

    なんとかなれ

--- test/test.py
+++ test/test.py
@@ -3,41 +3,60 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import sys
+import os
+import fcntl
 import time
 import subprocess
 
-player1 = subprocess.popen(["ros2", "run", "pkg_kadai", "player1"],
+#非ブロッキング処理？
+def non_blocking(file):
+    fd = file.fileno()
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+
+player1 = subprocess.Popen(["ros2", "run", "pkg_kadai", "player1"],
                      stdin=subprocess.PIPE,
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE,
                      text=True
                      )
 time.sleep(1)
-player2 = subprocess.popen(["ros2", "run", "pkg_kadai", "player2"],
+player2 = subprocess.Popen(["ros2", "run", "pkg_kadai", "player2"],
                      stdin=subprocess.PIPE,
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE,
                      text=True
                      )
 
-def text_inserter(input_text):
-    stdout, stderror = player1.communicate(input=input_text, timeout=10)
-    return stdout, stderror
+non_blocking(player1.stdout)
+non_blocking(player1.stderr)
+non_blocking(player2.stdout)
+
+def requester(input):
+    player1.stdin.write(input + "\n")
+    player1.stdin.flush()
     
+def reader():
+    time.sleep(1)
+    try:
+        return player2.stdout.read()
+    except:
+        return ""
 def main():
     
     try:
         time.sleep(2)
     
-        out_1, error_1 = text_inserter("aaa")[0]
-        out_2, error_2 = text_inserter("iii")[1]
-        
-        if "aaa" in out_1:
+        text = "aaiiuueeoo"
+        requester(text)
+        readed_text = reader()
+                
+        if text in readed_text:
             print("1おｋ")
         else:
             sys.exit("1ダメ")
             
-        if "iii" in out_2:
+        if text in readed_text:
             print("2おｋ")
         else:
             sys.exit("2ダメ")
@@ -54,13 +73,20 @@ def main():
     #以下例外処理
     
     #片方だけ起動して相手が存在しないエラーを吐くか
-    out_1_a, error_1_a = text_inserter("a")
+    errormsg = "No connection with partner"
     
-    if error_1_a[1] in "No connection with partner":
-        print("ok")
+    output = player1.stdout.read() 
+    error = player1.stderr.read() 
+    all_output = output + error
+        
+    if errormsg in all_output:
+        print("ステータス:ok")
     else:
-        sys.exit("Status check error")
+        sys.exit("status chech failed")
+            
     
-    
+    print("おわり")
+    player1.kill
+    player2.kill
 main()
     
