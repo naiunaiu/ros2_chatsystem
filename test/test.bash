28176c18ac63de8fb088eb0735af4420d4bc37f9
naiu <imagenaru2@gmail.com>
2026-01-09 21:20:01 +0900

    test.bash初版

--- test/test.bash
+++ test/test.bash
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Satoh-Narumi
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-timeout 10 ros2 launch pkg_kadai player1.py > /tmp/pkg_kadai.log
 
-cat /tmp/pkg_kadai.log | grep 
+echo 'taste' | timeout 10 ros2 launch pkg_kadai player1.py > /tmp/pkg_kadai.log
+cat /tmp/pkg_kadai.log | grep 'player1:taste'
