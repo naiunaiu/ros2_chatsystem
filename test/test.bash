fd76cd97c104fb5d1e686452f4f149f54bee3a94
naiu <imagenaru2@gmail.com>
2026-01-11 20:12:00 +0900

    でばっぎ6

--- test/test.bash
+++ test/test.bash
@@ -12,13 +12,10 @@ colcon build
 source install/setup.bash
 
 echo 'taste' | timeout 10 ros2 run pkg_kadai player1 > /tmp/pkg_kadai.log
+cat /tmp/pkg_kadai.log | grep 'player1:No connection with partner'
+sleep 1
 echo 'test' | timeout 10 ros2 run pkg_kadai2 player2 >> /tmp/pkg_kadai2.log
 echo 'torst' | timeout 10 ros2 run pkg_kadai3 player3 >> /tmp/pkg_kadai3.log
 cat /tmp/pkg_kadai.log | grep 'player1:taste'
 cat /tmp/pkg_kadai2.log | grep 'player2:test'
 cat /tmp/pkg_kadai3.log | grep 'player3:torst'
-
-#以下例外処理
-
-timeout 10 ros2 run pkg_kadai player1 >> /tmp/pkg_kadai.log
-cat /tmp/pkg_kadai.log | grep 'player1:No connection with partner'
