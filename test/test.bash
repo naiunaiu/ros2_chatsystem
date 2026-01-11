1e5efda08e06d51a56751801b2b4e68c5442e176
naiu <imagenaru2@gmail.com>
2026-01-11 16:29:53 +0900

    tst3

--- test/test.bash
+++ test/test.bash
@@ -6,6 +6,14 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd /root/ros2_ws
+cp src/pkg_kadai/pkg_kadai/player1.py src/pkg_kadai/pkg_kadai/player2.py
+cd /root/ros2_ws
+colcon build
+source install/setup.bash
 
-echo 'taste' | timeout 10 ros2 launch pkg_kadai player1 > /tmp/pkg_kadai.log
+echo 'taste' | timeout 10 ros2 run pkg_kadai player1 > /tmp/pkg_kadai.log
+echo 'test' | timeout 10 ros2 run pkg_kadai player2 > /tmp/pkg_kadai.log
 cat /tmp/pkg_kadai.log | grep 'player1:taste'
+cat /tmp/pkg_kadai.log | grep 'player2:test'
+
+rm src/pkg_kadai/pkg_kadai/player2.py
