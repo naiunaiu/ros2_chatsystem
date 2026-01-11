727ff95072950d1db28706c01df0dc4cc373d9d0
naiu <imagenaru2@gmail.com>
2026-01-11 17:17:43 +0900

    タイポ

--- test/test.bash
+++ test/test.bash
@@ -16,11 +16,9 @@ echo 'test' | timeout 10 ros2 run pkg_kadai2 player2 >> /tmp/pkg_kadai2.log
 echo 'torst' | timeout 10 ros2 run pkg_kadai3 player3 >> /tmp/pkg_kadai3.log
 cat /tmp/pkg_kadai.log | grep 'player1:taste'
 cat /tmp/pkg_kadai2.log | grep 'player2:test'
-cat /tmp/pkg_kadai2.log | grep 'player3:torsr'
+cat /tmp/pkg_kadai2.log | grep 'player3:torst'
 
 #以下例外処理
 
 timeout 10 ros2 run pkg_kadai player1 >> /tmp/pkg_kadai.log
 cat /tmp/pkg_kadai.log | grep 'player1:No connection with partner'
-
-rm src/pkg_kadai/pkg_kadai/player2.py
