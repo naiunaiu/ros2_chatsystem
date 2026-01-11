88e9de308acb18c18d5f4985b9f88ad722486166
naiu <imagenaru2@gmail.com>
2026-01-11 17:17:14 +0900

    test4

--- test/test.bash
+++ test/test.bash
@@ -6,14 +6,21 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd /root/ros2_ws
-cp src/pkg_kadai/pkg_kadai/player1.py src/pkg_kadai/pkg_kadai/player2.py
-cd /root/ros2_ws
+mv src/pkg_kadai/pkg_kadai2/player1.py player2.py
+mv src/pkg_kadai/pkg_kadai3/player1.py player3.py
 colcon build
 source install/setup.bash
 
 echo 'taste' | timeout 10 ros2 run pkg_kadai player1 > /tmp/pkg_kadai.log
-echo 'test' | timeout 10 ros2 run pkg_kadai player2 > /tmp/pkg_kadai.log
+echo 'test' | timeout 10 ros2 run pkg_kadai2 player2 >> /tmp/pkg_kadai2.log
+echo 'torst' | timeout 10 ros2 run pkg_kadai3 player3 >> /tmp/pkg_kadai3.log
 cat /tmp/pkg_kadai.log | grep 'player1:taste'
-cat /tmp/pkg_kadai.log | grep 'player2:test'
+cat /tmp/pkg_kadai2.log | grep 'player2:test'
+cat /tmp/pkg_kadai2.log | grep 'player3:torsr'
+
+#以下例外処理
+
+timeout 10 ros2 run pkg_kadai player1 >> /tmp/pkg_kadai.log
+cat /tmp/pkg_kadai.log | grep 'player1:No connection with partner'
 
 rm src/pkg_kadai/pkg_kadai/player2.py
