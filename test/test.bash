9a5ead4e06117dda62a3a0fd73dac06790810aa6
naiu <imagenaru2@gmail.com>
2026-01-12 12:19:12 +0900

    であっぎ8

--- test/test.bash
+++ test/test.bash
@@ -10,6 +10,8 @@ mv src/pkg_kadai2/pkg_kadai2/player1.py src/pkg_kadai2/pkg_kadai2/player2.py
 mv src/pkg_kadai3/pkg_kadai3/player1.py src/pkg_kadai3/pkg_kadai3/player3.py
 ls /root/ros2_ws/src/pkg_kadai2
 ls /root/ros2_ws/src/pkg_kadai3
+sed -i "s@'player1 = pkg_kadai.player1:main',@'player2 = pkg_kadai2.player2:main',@g" /root/ros2_ws/src/pkg_kadai2/setup.py
+sed -i "s@'player1 = pkg_kadai.player1:main',@'player3 = pkg_kadai3.player3:main',@g" /root/ros2_ws/src/pkg_kadai3/setup.py
 cd /root/ros2_ws
 colcon build
 source install/setup.bash
@@ -19,6 +21,6 @@ cat /tmp/pkg_kadai.log | grep 'player1:No connection with partner'
 sleep 1
 echo 'test' | timeout 10 ros2 run pkg_kadai2 player2 >> /tmp/pkg_kadai2.log
 echo 'torst' | timeout 10 ros2 run pkg_kadai3 player3 >> /tmp/pkg_kadai3.log
-cat /tmp/pkg_kadai.log | grep 'player1:taste'
-cat /tmp/pkg_kadai2.log | grep 'player2:test'
-cat /tmp/pkg_kadai3.log | grep 'player3:torst'
+cat /tmp/pkg_kadai.log | grep 'player1:torst'
+cat /tmp/pkg_kadai2.log | grep 'player2:taste'
+cat /tmp/pkg_kadai3.log | grep 'player3:test'
