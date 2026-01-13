3c572c4562c0a494fa42b81d9706364cf5ac7fce
naiu <imagenaru2@gmail.com>
2026-01-13 18:25:44 +0900

    2

--- test/test.bash
+++ test/test.bash
@@ -22,11 +22,12 @@ colcon build
 source install/setup.bash
 
 ros2 run pkg_kadai player1 > /tmp/pkg_kadai.log
-sleep 1
+sleep 2
+cat /tmp/pkg_kadai.log
 cat /tmp/pkg_kadai.log | grep 'No connection with partner' || ng "$LINENO"
-(sleep 1; echo "taste") | ros2 run pkg_kadai player1 > /tmp/pkg_kadai.log
-(sleep 1; echo "test") | ros2 run pkg_kadai2 player2 >> /tmp/pkg_kadai2.log
-(sleep 1; echo "torst") | ros2 run pkg_kadai3 player3 >> /tmp/pkg_kadai3.log
+(sleep 1; echo "taste") | ros2 run pkg_kadai player1 >> /tmp/pkg_kadai.log
+(sleep 1; echo "test") | ros2 run pkg_kadai2 player2 > /tmp/pkg_kadai2.log
+(sleep 1; echo "torst") | ros2 run pkg_kadai3 player3 > /tmp/pkg_kadai3.log
 sleep 2
 cat /tmp/pkg_kadai.log | grep 'player3>> torst' || ng "$LINENO"
 cat /tmp/pkg_kadai2.log | grep 'player1>> taste' || ng "$LINENO"
