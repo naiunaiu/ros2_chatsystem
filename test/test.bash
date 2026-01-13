44efdc77aae7c023e45a1ff15ce4096dc1eeb832
naiu <imagenaru2@gmail.com>
2026-01-13 20:32:04 +0900

    16

--- test/test.bash
+++ test/test.bash
@@ -12,11 +12,8 @@ res=0
 sudo apt-get install -y expect
 cd /root/ros2_ws
 mv src/pkg_kadai2/pkg_kadai2/player1.py src/pkg_kadai2/pkg_kadai2/player2.py
-mv src/pkg_kadai3/pkg_kadai3/player1.py src/pkg_kadai3/pkg_kadai3/player3.py
 ls /root/ros2_ws/src/pkg_kadai2
-ls /root/ros2_ws/src/pkg_kadai3
 sed -i "s@'player1 = pkg_kadai.player1:main',@'player2 = pkg_kadai2.player2:main',@g" /root/ros2_ws/src/pkg_kadai2/setup.py
-sed -i "s@'player1 = pkg_kadai.player1:main',@'player3 = pkg_kadai3.player3:main',@g" /root/ros2_ws/src/pkg_kadai3/setup.py
 cd /root/ros2_ws
 colcon build
 source install/setup.bash
@@ -29,9 +26,19 @@ expect -c "
   sleep 2
   send \"/exit\r\"
 " > /tmp/pkg_kadai.log 2>&1
+expect -c "
+  spawn ros2 run pkg_kadai2 player2
+  expect \"player2> \"
+  sleep 1
+  send \"test\r\"
+  sleep 2
+  send \"/exit\r\"
+" > /tmp/pkg_kadai2.log 2>&1
 
-cat /tmp/pkg_kadai.log | grep 'player1> torst' || ng "$LINENO"
+cat /tmp/pkg_kadai.log | grep 'player2>> test' || ng "$LINENO"
+cat /tmp/pkg_kadai2.log | grep 'player1>> taste' || ng "$LINENO"
 cat /tmp/pkg_kadai.log | grep 'No connection with partner' || ng "$LINENO"
 cat /tmp/pkg_kadai.log
+cat /tmp/pkg_kadai2.log
 echo OK
 exit $res
