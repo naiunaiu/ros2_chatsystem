06f8468982893100a02abc26e087187e008679de
naiu <imagenaru2@gmail.com>
2026-01-13 20:28:09 +0900

    15

--- test/test.bash
+++ test/test.bash
@@ -29,29 +29,9 @@ expect -c "
   sleep 2
   send \"/exit\r\"
 " > /tmp/pkg_kadai.log 2>&1
-expect -c "
-  spawn ros2 run pkg_kadai2 player2
-  expect \"player2> \"
-  sleep 1
-  send \"test\r\"
-  sleep 2
-  send \"/exit\r\"
-" > /tmp/pkg_kadai2.log 2>&1
-expect -c "
-  spawn ros2 run pkg_kadai3 player3
-  expect \"player3> \"
-  sleep 1
-  send \"torst\r\"
-  sleep 2
-  send \"/exit\r\"
-" > /tmp/pkg_kadai3.log 2>&1
 
-cat /tmp/pkg_kadai.log | grep 'player3>> torst' || ng "$LINENO"
-cat /tmp/pkg_kadai2.log | grep 'player1>> taste' || ng "$LINENO"
-cat /tmp/pkg_kadai3.log | grep 'player2>> test' || ng "$LINENO"
+cat /tmp/pkg_kadai.log | grep 'player1> torst' || ng "$LINENO"
 cat /tmp/pkg_kadai.log | grep 'No connection with partner' || ng "$LINENO"
 cat /tmp/pkg_kadai.log
-cat /tmp/pkg_kadai2.log
-cat /tmp/pkg_kadai3.log
 echo OK
 exit $res
